b2c4deb275559800b5b90babf354fac383fde3b7
Peter Kaberia <89773728+peterkaberia@users.noreply.github.com>
2026-04-13 22:20:37 +0300

    Environment set up for dashboard

--- R/setup.R
+++ R/setup.R
@@ -2,6 +2,8 @@ if (!require(pacman)) install.packages("pacman")
 
 pacman::p_load(
   dplyr, readxl, readr, haven,
-  openxlsx, ggplot2, tidyr, stringr, here)
+  openxlsx, ggplot2, tidyr, stringr, here,
+  flexdashboard, plotly, DT
+)
 
 options(stringsAsFactors = FALSE)
